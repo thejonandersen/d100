e8afa29b791adc4240c65434d3825df67dda0b9a
Jon Andersen <jonathan.andersen@htlkey.com>
2025-02-06 11:59:06 -0500

    missed 2 files

---
+++ web/src/components/form/useForm.ts
@@ -0,0 +1,73 @@
+import { useCallback, useEffect, useState } from "react";
+import { useAppSelector, useAppDispatch } from '../../state/hooks';
+import {mergedFormsData, registerForm, status, submitForm} from '../../state/form/slice';
+import useFormConditions from './useFormConditions'
+import z from 'zod'
+import {isEqual} from '../../common/utils'
+
+type FormProps = {
+    id: string;
+    submitData?: {
+        keys: string[];
+        url: string;
+        id?: string;
+        onComplete?: (success: boolean, data?: any) => void;
+    }
+    submit?: (data: any) => void;
+    initialData: any;
+    schema: z.ZodTypeAny;
+}
+
+type SubmitHandler = (() => void) | null
+
+const useForm = ({id, submitData, submit, initialData, schema}: FormProps) => {
+    const [registered, setRegistered] = useState<boolean>(false)
+    const [isValid, setIsValid] = useState<boolean>(false);
+    const formStatus = useAppSelector(status);
+    const mergedData = useAppSelector(mergedFormsData);
+    const [cachedData, setCachedData] = useState<any>(mergedData);
+    const [submitClickHandler, setSubmitClickHandler] = useState<SubmitHandler>(null);
+    const {registerConditions, conditions} = useFormConditions(id);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (!submit && !submitData || submitClickHandler)
+            return;
+
+        const handler = () => () => {
+            if (submit) {
+                submit(mergedData);
+            } else if (submitData) {
+                dispatch(submitForm({...submitData, data: mergedData}))
+            }
+        }
+
+        setSubmitClickHandler(handler);
+    }, [submit, submitData]);
+
+    useEffect(() => {
+        if (isEqual(mergedData, cachedData))
+            return;
+
+        setCachedData(mergedData);
+    }, [mergedData]);
+
+
+
+    useEffect(() => {
+        if (isEqual({}, mergedData))
+            return;
+
+        setIsValid(schema.safeParse(mergedData).success);
+    }, [mergedData]);
+
+    useEffect(() => {
+        dispatch(registerForm({id, data: initialData || {}}))
+        setRegistered(true);
+    }, []);
+
+
+    return {registered, isValid, registerConditions, formStatus, submitClickHandler, conditions};
+}
+
+export default useForm;
---
+++ web/src/components/form/useFormConditions.ts
@@ -0,0 +1,107 @@
+import {useCallback, useEffect, useState} from "react";
+import {useAppSelector} from '../../state/hooks'
+import {get, isEqual} from 'lodash'
+import {v4 as uuid} from 'uuid'
+
+type Option = {
+    [key: string]: string[]
+}
+
+export type RegisterConditionParams = {
+    key: string;
+    scope: string;
+    parent?: string;
+    options?: Option[];
+    equals?: string | number | boolean;
+    not?: string | number | boolean;
+    id: string;
+}
+
+export type Condition = {
+    path: string;
+    conditionMet?: boolean;
+    optionsMet?: string[] | null;
+    options?: Option[];
+    equals?: string | number | boolean | string[];
+    not?: string | number | boolean;
+}
+
+export type Conditions = {
+    [key: string]: Condition;
+}
+
+const useFormConditions = (formId: string) => {
+    const [id] = useState<string>(formId);
+    const [ids, setIds] = useState<string[]>([]);
+    const [conditions, setConditions] = useState<Conditions>({});
+    const formData = useAppSelector(state => state.form.forms[id]);
+
+    const registerConditions = (conditionParams: RegisterConditionParams[]) => {
+        let newConditions: Conditions = {}
+        let newIds: string[] = [];
+        let componentIds: string[] = [];
+        conditionParams.forEach(condition => {
+            const {key, scope, parent, options, equals, not, id} = condition;
+            componentIds.push(id);
+            if (!ids.includes(id)) {
+                newIds.push(id);
+            }
+            newConditions[id] = {
+                path: scope === 'parent' ? `${parent}.${key}` : key as string,
+                options,
+                equals,
+                not,
+                conditionMet: false,
+                optionsMet: null,
+            }
+        })
+
+        if (newIds.length) {
+            setIds(prevState => [...prevState, ...newIds]);
+            setConditions(prevState => ({...prevState, ...newConditions}));
+        }
+
+        return componentIds;
+    };
+
+    useEffect(() => {
+        if (!Object.keys(conditions).length || !formData) return;
+
+        const newConditions = Object.keys(conditions).reduce((pre, id) => {
+            const condition = conditions[id];
+            const {path, options, equals, not} = condition;
+            const value: string | number | boolean = get(formData, path);
+            let conditionMet = false;
+            let optionsMet = null;
+            if (equals) {
+                if (Array.isArray(equals)) {
+                    conditionMet = equals.includes(value as string);
+                }
+                conditionMet = value === equals;
+            } else if (not) {
+                conditionMet = value !== not;
+            }
+
+            if (options) {
+                optionsMet = get(options, value as string);
+            }
+
+            return {
+                ...pre,
+                [id]:{
+                    ...condition,
+                    conditionMet,
+                    optionsMet,
+                }
+            }
+        }, {})
+
+        if (!isEqual(newConditions, conditions))
+            setConditions(newConditions);
+
+    }, [conditions, formData]);
+
+    return {registerConditions, conditions}
+}
+
+export default useFormConditions;
